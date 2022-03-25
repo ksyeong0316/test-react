2c3d51344edf791eccf1f08d3f8c9ccc8c5eb8c6
ksyeong0316 <ksyoeng0316@daum.net>
2022-03-25 15:03:17 +0900

    test1

--- src/Person.js
+++ src/Person.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
-function Person({name, age}) {
-  return (
-  	<>
-    	  <h3>{name}</h3>
-          <h4>{age}</h4>
-       </>
-  )
+class Person extends React.Component {
+  state = {
+      name: "sunrise",
+      age: 23,
+      friends: [
+          "victoria",
+          "daniel",
+          "hanna"
+      ]
+  }
+  
+  render() {    
+    return (
+    	<>
+            <button onClick={this.displayInfo}>신상정보 확인하기</button>
+        </>
+    )
+  }
 }
 
 export default Person;
