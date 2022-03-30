c68c0c7fb6ec729cc4bc3ee2bba2591099540e0e
ksyeong0316 <ksyoeng0316@daum.net>
2022-03-30 15:52:37 +0900

    changed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NameTag from './NameTag'
 function App() {
   return (
     <>
+    <h1>변경됨</h1>
       <NameTag type='dog' name='meyow' size='small' sound='low' appearence='cute'/>
     </>
   );
