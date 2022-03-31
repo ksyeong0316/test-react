7be8f2e9ccf2a1a25bc86ff9abb137a35f458c35
ksyeong0316 <ksyoeng0316@daum.net>
2022-03-31 15:14:38 +0900

    vscode commit test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import NameTag from './NameTag'
+// import { useEffect } from 'react'
 
 function App() {
+  // useEffect(()=>{
+  //   setInterval(()=>{
+
+  //   }, 1000/60)
+  // })
+  
   return (
     <>
     <h1>변경됨</h1>
+    <h1>변경됨1</h1>
       <NameTag type='dog' name='meyow' size='small' sound='low' appearence='cute'/>
     </>
   );
